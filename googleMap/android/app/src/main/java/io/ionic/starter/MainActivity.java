f210cd484c1de8c86ebdbb41d960e4400606ffbb
Nathalie <nafu@hotmail.com>
2021-12-07 18:39:17 +0000

    commit

--- googleMap/android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ googleMap/android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -2,4 +2,10 @@ package io.ionic.starter;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        registerPlugin(CapacitorGoogleMaps.class);
+    }
+}
