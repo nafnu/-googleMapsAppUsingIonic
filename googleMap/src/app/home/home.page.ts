1a6a1b9cf4d9c0d1ab543201993748993dba49de
Nathalie <nafu@hotmail.com>
2021-12-07 18:52:04 +0000

    commit

--- googleMap/src/app/home/home.page.ts
+++ googleMap/src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  @ViewChild('map') mapView: ElementRef;
 
   constructor() {}
 
