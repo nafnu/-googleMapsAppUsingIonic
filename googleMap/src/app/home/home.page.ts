d33f9b7a84f7059b793bfd950ed3ce3ce3f1b080
Nathalie <nafu@hotmail.com>
2021-12-08 14:18:00 +0000

    commit

--- googleMap/src/app/home/home.page.ts
+++ googleMap/src/app/home/home.page.ts
@@ -2,6 +2,7 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 import { CapacitorGoogleMaps } from '@capacitor-community/capacitor-googlemaps-native';
 import { Capacitor } from '@capacitor/core';
 import { Geolocation } from '@capacitor/geolocation';
+import { LatLng } from '@capacitor-community/capacitor-googlemaps-native/dist/esm/types/common/latlng.interface';
 
 @Component({
   selector: 'app-home',
@@ -20,16 +21,16 @@ export class HomePage {
 
   createMap() {
     const boundingRect = this.mapView.nativeElement.getBoundingClientRect() as DOMRect;
-    console.log("file: home.page.ts ~ line 20 ~ HomePage ~ createMap ~ boundingRect", boundingRect)
+    console.log("~ file: home.page.ts ~ line 20 ~ HomePage ~ createMap ~ boundingRect", boundingRect)
 
     CapacitorGoogleMaps.create({
       width: Math.round(boundingRect.width),
       height: Math.round(boundingRect.height),
       x: Math.round(boundingRect.x),
       y: Math.round(boundingRect.y),
-      latitude: -33.86,
-      longitude: 151.20,
-      zoom: 12
+      // latitude: -33.86,
+      // longitude: 151.20,
+      zoom: 5
     });
 
     CapacitorGoogleMaps.addListener("onMapReady", async () => {
@@ -45,10 +46,40 @@ export class HomePage {
   showCurrentPosition(){
 
     Geolocation.requestPermissions().then(async permission => {
+      const coordinates  = await Geolocation.getCurrentPosition();
 
-      const coordinates = await Geolocation.getCurrentPosition();
-    })
+      CapacitorGoogleMaps.addMarker({
+        latitude: coordinates.coords.latitude,
+        longitude: coordinates.coords.longitude,
+        title: 'My place',
+        snippet: 'I am here'
+      });
+
+      CapacitorGoogleMaps.setCamera({
+        latitude: coordinates.coords.latitude,
+        longitude: coordinates.coords.longitude,
+        zoom: 12,
+        bearing: 0
+      });
+    });
   }
-  
 
+  draw(){
+    const points: LatLng[] = [
+      {
+        latitude: 51.88,
+        longitude: 7.60,
+      },
+      {
+        latitude: 55,
+        longitude: 10,
+      }
+    ];
+
+    CapacitorGoogleMaps.addPolyline({
+      points,
+      color: '#ff00ff',
+      width: 2
+    });
+  }
 }
